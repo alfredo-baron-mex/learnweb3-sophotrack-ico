5a7259e9877d947085a13925e68cf3f4f4117f2b
Alfredo Baron <alfredo.baron@gmail.com>
2022-03-23 12:47:20 -0600

    Initial commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xC6b26f70b366A79E1444dbCEcFEab6b044bD16C6";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
